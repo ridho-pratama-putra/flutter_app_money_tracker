e188bde3fbd34cd484282527c6143a5abda9456e
ridho <ridho.putra@btpn.com>
2022-07-11 14:25:27 +0700

    add widget to show monthlySummary

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'CurrencyFormat.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -29,10 +31,58 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       key: const Key('scaffold'),
       appBar: AppBar(
-        title: const Text('money tracker'),
+        title: const Text('Money Tracker'),
       ),
-      body: const Center(
-        child: Text('content'),
+      body: Container(
+        child: Column(
+          children: const <Widget>[
+            Card(
+              elevation: 15,
+              child: MonthlySummary(amount: 20000),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MonthlySummary extends StatelessWidget {
+  final int amount;
+
+  const MonthlySummary({Key? key, required this.amount}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          stops: [0.01, 0.5, 0.6, 0.9],
+          colors: [
+            Colors.purple.shade200,
+            Colors.purple.shade700,
+            Colors.purple.shade600,
+            Colors.purple.shade400,
+          ],
+        ),
+      ),
+      height: 200,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Total Spended',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          Text(
+            CurrencyFormat.convertToIdr(amount, 0),
+            style: const TextStyle(fontSize: 34, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
