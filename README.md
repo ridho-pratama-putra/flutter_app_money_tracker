eb9c58b59770931e0ae0b32fc01d097dfb09e94e
ridho <ridho.putra@btpn.com>
2022-07-11 07:11:39 +0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_app_money_tracker
+
+A new Flutter project.
+
+## notes
+add flutter_launcher_icons dependency and add image at assets/icon
+then run this command to set created app icon in all platform
+```flutter pub run flutter_launcher_icons:main```
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: const Key('scaffold'),
+      appBar: AppBar(
+        title: const Text('money tracker'),
+      ),
+      body: const Center(
+        child: Text('content'),
+      ),
+    );
+  }
+}
