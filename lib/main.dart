359719f36df4b5ce22b89909532273f5f8a57ab1
ridho <ridhopratamavd@gmail.com>
2022-07-21 22:26:53 +0700

    add SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,15 @@ class MyHomePage extends StatelessWidget {
     Transaction('iw', 'charger mac', 21000, DateTime.now()),
     Transaction('ir', 'gojek', 21000, DateTime.now()),
     Transaction('it', 'grab', 21000, DateTime.now()),
+    Transaction('i4', 'ovo', 21000, DateTime.now()),
+    Transaction('i5', 'dana', 21000, DateTime.now()),
+    Transaction('i6', 'logi', 21000, DateTime.now()),
+    Transaction('i7', 'tech', 21000, DateTime.now()),
+    Transaction('i8', 'club', 21000, DateTime.now()),
+    Transaction('i9', 'car', 21000, DateTime.now()),
+    Transaction('i1', 'motorcycle', 21000, DateTime.now()),
+    Transaction('i2', 'iphone', 21000, DateTime.now()),
+    Transaction('i3', 'android', 21000, DateTime.now()),
   ];
 
   MyHomePage({Key? key}) : super(key: key);
@@ -44,101 +53,120 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Money Tracker'),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            const Card(
-              elevation: 15,
-              child: MonthlySummary(amount: 20000),
-            ),
-            Column(
-              children: [
-                Container(
-                  alignment: Alignment.topLeft,
-                  padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
-                  child: const Text(
-                    "July",
-                    style: TextStyle(fontSize: 20, color: Colors.grey),
-                  ),
-                )
-              ],
-            ),
-            Column(
-              children: transaction
-                  .map((e) => Container(
-                        height: 65,
-                        width: double.infinity,
-                        key: Key(e.id),
-                        decoration: const BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: Card(
-                            elevation: 2,
-                            key: Key(e.id),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              const Card(
+                elevation: 15,
+                child: MonthlySummary(amount: 20000),
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    const TextField(
+                        decoration: InputDecoration(label: Text('Title'))),
+                    const TextField(
+                        decoration: InputDecoration(label: Text('Amount'))),
+                    TextButton(
+                        onPressed: () => {},
+                        child: const Text(
+                          'Submit',
+                          style: TextStyle(color: Colors.deepPurpleAccent),
+                        ))
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                child: const Text(
+                  "July",
+                  style: TextStyle(fontSize: 20, color: Colors.grey),
+                ),
+              ),
+              Column(
+                children: transaction
+                    .map((e) => Container(
+                          height: 65,
+                          width: double.infinity,
+                          key: Key(e.id),
+                          decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          child: Card(
+                              elevation: 2,
+                              key: Key(e.id),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          15, 0, 0, 0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          CircleAvatar(
+                                            maxRadius: 15,
+                                            child: Text(
+                                              DateFormat.d().format(e.date),
+                                              style:
+                                                  const TextStyle(fontSize: 9),
+                                            ),
+                                          ),
+                                          Container(
+                                            padding: const EdgeInsets.fromLTRB(
+                                                15, 0, 0, 0),
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  e.title.toUpperCase(),
+                                                  style: const TextStyle(
+                                                    fontSize: 17,
+                                                    color: Colors.grey,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  DateFormat.jm()
+                                                      .format(e.date),
+                                                  style: const TextStyle(
+                                                    fontSize: 10,
+                                                    color: Colors.grey,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      )),
+                                  Container(
                                     padding:
-                                        const EdgeInsets.fromLTRB(15, 0, 0, 0),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        CircleAvatar(
-                                          child: Text(
-                                            DateFormat.d().format(e.date),
-                                            style: TextStyle(fontSize: 9),
-                                          ),
-                                          maxRadius: 15,
-                                        ),
-                                        Container(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              15, 0, 0, 0),
-                                          child: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                e.title.toUpperCase(),
-                                                style: const TextStyle(
-                                                  fontSize: 17,
-                                                  color: Colors.grey,
-                                                ),
-                                              ),
-                                              Text(
-                                                DateFormat.jm().format(e.date),
-                                                style: const TextStyle(
-                                                  fontSize: 10,
-                                                  color: Colors.grey,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    )),
-                                Container(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(0, 0, 15, 0),
-                                  child: Text(
-                                    CurrencyFormat.convertToIdr(e.amount, 0),
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.grey,
+                                        const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                                    child: Text(
+                                      CurrencyFormat.convertToIdr(e.amount, 0),
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.grey,
+                                      ),
                                     ),
-                                  ),
-                                )
-                              ],
-                            )),
-                      ))
-                  .toList(),
-            )
-          ],
+                                  )
+                                ],
+                              )),
+                        ))
+                    .toList(),
+              )
+            ],
+          ),
         ),
       ),
     );
